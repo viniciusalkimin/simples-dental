0eca91dda4074bbae1cd67b5d212de907f08ef49
viniciusalkimin <viniciusg_alkimin@hotmail.com>
2024-03-07 16:22:22 -0300

    Creation of base project

---
+++ src/main/java/com/alkimin/simplesdental/SimplesDentalApplication.java
@@ -0,0 +1,13 @@
+package com.alkimin.simplesdental;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimplesDentalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimplesDentalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alkimin/simplesdental/SimplesDentalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alkimin.simplesdental;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimplesDentalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
